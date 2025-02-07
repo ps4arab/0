b35dfd361bc8a7bd89361c90c0e79932d3943959
ps4arab <162325520+ps4arab@users.noreply.github.com>
2025-02-07 23:59:38 +0100

    Delete password.js

--- password.js
+++
@@ -1,9 +0,0 @@
-window.onload=passwordCheck;
-function passwordCheck()
-{
-    var password = prompt("Please enter the password. Otherwise contact us: imadaliandroid@gmail.com");
-    if (password !== "1234AB@")
-    {
-        passwordCheck();
-    }
-}
