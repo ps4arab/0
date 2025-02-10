b8cf1958e649a574e4df5b9771c032ed1150d668
ps4arab <162325520+ps4arab@users.noreply.github.com>
2025-02-10 18:18:52 +0100

    Add files via upload

--- 0/0/premiumpassword.js
+++ 0/0/premiumpassword.js
@@ -2,8 +2,35 @@ window.onload=passwordCheck;
 function passwordCheck()
 {
     var password = prompt("Please enter the password. Otherwise contact us: imadaliandroid@gmail.com");
+    if (password != "1234AB@"){
+setTimeout(15000)
+alert ("Wrong password! Time Out!");
+document.getElementsByTagName("BODY")[0].style.display = "none";
+} else {
+return true;
+  }
+}
+
+/* the already working one
+window.onload=passwordCheck;
+function passwordCheck()
+{
+    var password = prompt("Please enter the password. Otherwise contact us: imadaliandroid@gmail.com");
     if (password !== "1234AB@")
     {
         passwordCheck();
     }
-}
+}}*/
+
+
+/*
+window.onload=passwordCheck;
+function password_protect() {
+var pwd = prompt ("Enter the password", "");
+if (pwd != 123) {
+alert ("Wrong password!");
+return false;
+} else {
+return true;
+  }
+}*/
